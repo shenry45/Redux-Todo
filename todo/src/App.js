5360d57048b90a7fb6c0a6675b34d7c7ff6e3811
shenry45 <shawn45henry@gmail.com>
2019-06-21 18:42:27 -0700

    CSS update

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ToDoList from './components/ToDoList';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <div className="App">
+    <AppEl className="App">
+      <h1>Today's To Do List</h1>
       <ToDoList />
-    </div>
+    </AppEl>
   );
 }
+
+const AppEl = styled.div`
+  width: 700px;
+  margin: 0 auto;
+
+  h1 {
+    text-align: center;
+  }
+`;
 
 export default App;
--- todo/src/components/ToDo.js
+++ todo/src/components/ToDo.js
@@ -25,12 +25,13 @@ const ToDo = (props) => {
 }
 
 const ToDoEl = styled.div`
+  cursor: pointer;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   align-items: center;
   margin-bottom: 20px;
-  padding: 20px;
+  padding: 10px 40px;
   border: 1px solid #cacaca;
   border-radius: 10px;
   background: #f5f5f5;
@@ -44,8 +45,11 @@ const ToDoEl = styled.div`
   }
 
   p {
+    font-size: 1.25rem;
     margin: 0 20px 0 0;
+    word-break: break-word;
   }
+  
   button {
     min-width: 75px;
     margin-right: 10px;
